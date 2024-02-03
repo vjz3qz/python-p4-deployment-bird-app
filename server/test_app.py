4ed8794edeb4c2d87405d115a2bdaaa5130bf40c
vjz3qz <vjz3qz@virginia.edu>
2024-02-03 15:00:07 -0500

    test file

---
+++ server/test_app.py
@@ -0,0 +1,10 @@
+# test_app.py
+
+import pytest
+from app import add_numbers
+
+
+def test_add_numbers():
+    assert add_numbers(1, 2) == 3
+    assert add_numbers(-1, 1) == 0
+    assert add_numbers(0, 0) == 0
